51b4fa59e6af5f3f4c7747cf18b3ff26f21a8266
Łukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2022-04-04 22:37:04 +0200

    added final opts and sorted

--- btaf.sh
+++ btaf.sh
@@ -5,31 +5,48 @@ dir_path='btaf-dir'
 declare -i passed_count=0
 declare -i tests_count=0
 
-#opts
-default_program='main' #default mode, testing main program
-t_flag='false'; test_to_run='None' #run particular test
-verbose='false' #full output from diff
-h_flag='false' #displaying help
-c_flag='false' #command mode
-n_flag='false' #creating new test
-l_flag='false' #listing tests
-i_flag='false' #interactive mode
-e_flag='false' #edit mode
-u_flag='false' #uninstall btaf
-d_flag='true' #default mode
+#defaults
+program_to_test='main' #default mode - testing main program
 
-while getopts ':hcvndliet:f:' flag; do
+#opts
+a_flag='false' #all
+c_flag='false' #command mode
+d_flag='false' #delete test
+e_flag='false' #edit mode
+h_flag='false' #displaying help
+i_flag='false' #interactive mode (prompting)
+l_flag='false' #listing tests
+m_flag='false' #showing man page
+n_flag='false' #creating new test
+p_flag='false' #program to test
+s_flag='false' #save test to zip
+t_flag='false' #run particular test, default lastest created, accepts array of tests
+u_flag='false' #uninstall btaf
+v_flag='false' #verbose
+V_flag='false' #print version
+x_flag='false' #crosstest mode
+z_flag='false' #unzip tests 
+
+
+while getopts ':acd:e:hilmnp:s:t:uvVx:z:' flag; do
   case "${flag}" in
-    h) h_flag='true';;
+    a) a_flag='true';;
     c) c_flag='true';;
-    v) verbose='true';;
-    n) n_flag='true';;
-    d) d_flag='true';;
-    l) l_flag='true';;
+    d) d_flag='true'; test_to_delete="${OPTARG}";;
+    e) e_flag='true'; test_to_edit="${OPTARG}";;
+    h) h_flag='true';;
     i) i_flag='true';;
-    e) e_flag='true';;
+    l) l_flag='true';;
+    m) m_flag='true';;
+    n) n_flag='true';;
+    p) p_flag='true'; program_to_test="${OPTARG}";;
+    s) s_flag='true'; test_to_save="${OPTARG}";;
     t) t_flag='true'; test_to_run="${OPTARG}";;
-    f) default_program="${OPTARG}" ;;
+    u) u_flag='true';;
+    v) v_flag='true';;
+    V) V_flag='true';;
+    x) x_flag='true'; program_to_crosstest="${OPTARG}";;
+    z) z_flag='true'; file_to_unzip="${OPTARG}";;
     \?) tput setaf 1; echo -e "Illegal option $*"; tput sgr0;
         echo -e "---- Displaying help message ----"
         h_flag='true'
@@ -42,7 +59,7 @@ default_mode (){
     for test in "$dir_path"/in/*.in; do
         tests_count+=1
         testname="$(basename "${test}")"
-        if [ "$verbose" == "true" ] && \
+        if [ "$v_flag" == "true" ] && \
              (tput setaf 1; diff -w <(./"$default_program" <"$test" ) "$dir_path/out/${testname:0:(-3)}.out")  || \
              diff -w <(./"$default_program" <"$test" ) "$dir_path/out/${testname:0:(-3)}.out" > /dev/null
          then
@@ -60,7 +77,7 @@ cmd_mode (){
         tests_count+=1
         chmod +x "$cmd"
         testname="$(basename "${cmd}")"
-        if [ "$verbose" == "true" ] && \
+        if [ "$v_flag" == "true" ] && \
             (tput setaf 1; diff -w <(eval "${cmd}") "$dir_path/out/${testname:0:(-3)}.out")  || \
             diff -w <(eval "${cmd}") "$dir_path/out/${testname:0:(-3)}.out" > /dev/null
         then
